8466f2ec32cb044c10c838e25c1a289e74d8c7ce
Ricardo Dias Cavalcante <ricardo.cavalcante@tst.jus.br>
2015-11-24 16:05:55 -0200

    Atualizando estrutura

---
+++ client/jarvis-proxy-http-client.js
@@ -0,0 +1,58 @@
+var http = require("http");
+var https = require("https");
+var request = require("request");
+var url = require("url");
+var tls = require('tls');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var server = http.createServer(function(req, resp){
+    var destHost =  req.headers.host;
+    var destHeaders = req.headers;
+    var destMethod = req.method;
+    var destUrl = url.parse(req.url);
+
+
+    var options = {
+            //Your proxy server
+            host: '127.0.0.1',
+            //Your proxy port
+            port: 3128,
+
+            path  : destUrl.href,
+            method: destMethod,
+            // method: 'CONNECT',
+            headers: destHeaders
+    };
+
+
+    console.log("\tHost: "+ destHost);
+    console.log("\tMethod: "+ destMethod);
+    console.log("\tUrl: "+ destUrl.href);
+    // console.log(JSON.stringify(options));
+    var destino = http.request(options, function(r){
+    // var destino = http.request(options, function(r){
+            console.log("Response");
+            resp.writeHead(r.headers);
+
+             r.on('data', function(chunk){
+                 resp.write(chunk);
+             });
+             r.on('end', function(){
+                console.log("r.end");
+                resp.end();
+             });
+
+             r.on('error',function(err){
+                console.log("Request Error : "+err);
+             });
+
+
+    });
+
+    destino.end();
+});
+
+server.listen(3131);
+
+console.log("> Servidor de pé.");
---
+++ experiments/internet/proxy-request-https.js
@@ -0,0 +1,35 @@
+//http://blog.vanamco.com/proxy-requests-in-node-js/
+
+var Http = require('http');
+var Tls = require('tls');
+// var sslrootcas = require('ssl-root-cas');
+// sslrootcas.inject().addFile("/home/ricardo/dias/node/jarvis-prox/keys/proxyserver.rede.tst.cer")
+// .addFile("/etc/ssl/certs/ca-certificates.crt");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var req = Http.request({
+    host: '127.0.0.1',
+    port: 3128,
+    method: 'CONNECT',
+    path: 'https://google.com:443'
+});
+
+req.on('connect', function (res, socket, head) {
+    var options = {
+        host: 'google.com',
+        socket: socket
+    };
+
+    var cts = Tls.connect(options, function () {
+        cts.write('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n');
+    });
+
+    // console.log(res.connection.getPeerCertificate);
+
+    cts.on('data', function (data) {
+        console.log(data.toString());
+    });
+});
+
+req.end();
